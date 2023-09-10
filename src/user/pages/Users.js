c9fbd4fdb7992dda76770f7a8437844ee7d6db2b
sanjeev <sanjeevsheoran07@gmail.com>
2023-09-10 06:05:50 +0530

    changes

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "sanjeev",
+      name: "Sanjeev Kumar",
       image:
         "https://i.pinimg.com/1200x/26/1e/9e/261e9e461b7e09537fed22e4d04cc19c.jpg",
       places: "5",
